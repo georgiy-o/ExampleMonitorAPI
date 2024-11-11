315cc575515959bc003c07922297582e46830f50
Heorhii Olenchenko <georgiy.olenchenko@gmail.com>
2024-11-11 13:28:36 +0000

    Created script for deployment go api

---
+++ setup_go_api.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+if ! dpkg -l | grep -q "golang" || ! go version | grep -q "linux/amd64"; then
+    printf "Error! Golang is not installed. Installing from longsleep repository...\n"
+    sudo add-apt-repository ppa:longsleep/golang-backports
+    sudo apt update
+    sudo apt install golang-go
+fi
+
+printf "Building project...\n"
+sudo mkdir -p /opt/monitor_api
+sudo mkdir -p /opt/monitor_api/pub
+sudo cp ./index.html /opt/monitor_api/pub/index.html
+go mod tidy
+go mod vendor
+go build -o /opt/monitor_api/monitor_api ./
+sudo touch /opt/monitor_api/monitors.txt
+sudo echo '123,"LG 27GL850"' > /opt/monitor_api/monitors.txt
+sudo echo '456,"Dell U2720Q"' >> /opt/monitor_api/monitors.txt
+sudo echo '789,"Samsung LU28R550"' >> /opt/monitor_api/monitors.txt
+sudo chmod +x /opt/monitor_api/monitor_api
+cd /opt/monitor_api || exit
+sudo ./monitor_api --createdb
+if sudo ./monitor_api --start 2>&1 | grep -q "Failed to start server"; then
+    printf "Cannot start API! Check if port 8030 is open!\n"
+else
+    printf "API started!\n"
+fi
